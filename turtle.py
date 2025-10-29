04f92c83bae5f5efcb5f95d735ec1726fcbac958
Renato Filho <renatofilho3344@gmail.com>
2025-10-29 15:45:15 -0300

    Atualizar o turtle.py

--- turtle.py
+++ turtle.py
@@ -1,1 +1,15 @@
+import turtle
+from time import sleep
 
+t = turtle.Turtle()
+
+def Draw_Shape:
+   t.foward(50)
+   t.right(90)
+
+t.beginfill()
+
+for i in range(4):
+   Draw_Shape
+
+t.endfill()
